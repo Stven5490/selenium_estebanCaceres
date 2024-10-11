7b598525da2388d4348c4b402232d0f9c1f9fce4
Stven5490 <est.caceres@gmail.com>
2024-10-11 09:52:42 -0300

    Controller de WebDriver

--- src/test/java/constantes.java
+++ src/test/java/constantes.java
@@ -3,13 +3,18 @@ public class constantes {
     private static String driver = "\\src\\test\\resources\\driveres\\";
     private static String chrome = "chromedriver.exe";
     private static String edge = "msedgedriver.exe";
+    private static String firefox = null;
     private static String chromeDriver = rutaProyecto + chrome;
     private static String edgeDriver = rutaProyecto + edge;
+    private static String fireFoxDriver = rutaProyecto + firefox;
     public static String obtenerChromeDriver(){
         return chromeDriver;
     }
 
     public static String obtenerEdgeDriver() {
         return edgeDriver;
+    }
+    public static String obtenerFireFoxDriver(){
+        return fireFoxDriver;
     }
 }
